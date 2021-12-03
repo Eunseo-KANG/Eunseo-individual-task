10564813f61f9ab815dd8dcae780ecdd2f2a2096
Eunseo-KANG <92796869+Eunseo-KANG@users.noreply.github.com>
2021-12-03 19:58:44 +0900

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const User = require('./User');
+const Coin = require('./Coin');
+const Balance = require('./Balance');
+const Key = require('./Key');
+
+const mongoURL =
+  'mongodb+srv://test0:test0@testmongo.kfy77.mongodb.net/coinServer?retryWrites=true&w=majority';
+mongoose.connect(mongoURL);
+
+module.exports = {
+  User,
+  Coin,
+  Balance,
+  Key,
+};
